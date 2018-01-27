7896dad5f22a1f39b535293f05679c0e95458956
kallifabio <kalli190901@gmail.com>
2018-01-28 00:53:00 +0100

    AFK-Plugin

--- src/de/kallifabio/afk/utils/ConfigManager.java
+++ src/de/kallifabio/afk/utils/ConfigManager.java
@@ -11,54 +11,65 @@ public class ConfigManager {
 	public static final File configFile = new File("plugins//AFK//config.yml");
 	public static final FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
 	
-	public static void setLangague(String langague) {
+	static String langague = "de ";
+	static String messagede = "§4DE ";
+	static String messageen = "§cEN ";
+	static String messagefr = "§3FR ";
+	
+	public static void setDefaults() {
+		config.set("langague", langague);
+		config.set("message.de", messagede);
+		config.set("message.en", messageen);
+		config.set("message.fr", messagefr);
+		saveConfig();
+		config.getString("langague");
+		config.getString("message.de");
+		config.getString("message.en");
+		config.getString("message.fr");
+	}
+	
+	public static void setLangague() {
 		config.set("langague", langague);
 		saveConfig();
 	}
-
+	
 	public static String getLangague() {
-		return config.getString("langague");
+		return config.getString(langague);
 	}
 	
-	public static void setMessageDE(String messagede) {
+	public static void setMessageDE() {
 		config.set("message.de", messagede);
 		saveConfig();
 	}
-
-	private static String getMessageDE() {
-		return config.getString("message.de");
+	
+	public static String getMessageDE() {
+		return config.getString(messagede);
 	}
-
-	public static void setMessageEN(String messageen) {
+	
+	public static void setMessageEN() {
 		config.set("message.en", messageen);
 		saveConfig();
 	}
-
-	private static String getMessageEN() {
-		return config.getString("message.en");
+	
+	public static String getMessageEN() {
+		return config.getString(messageen);
 	}
 	
-	public static void setMessageFR(String messagefr) {
+	public static void setMessageFR() {
 		config.set("message.fr", messagefr);
 		saveConfig();
 	}
-
-	private static String getMessageFR() {
-		return config.getString("message.fr");
+	
+	public static String getMessageFR() {
+		return config.getString(messagefr);
 	}
-
-	public static void setDefaults() {
-		config.set("langague", getLangague());
-		config.set("message.de", getMessageDE());
-		config.set("message.en", getMessageEN());
-		config.set("message.fr", getMessageFR());
-		saveConfig();
-	}
-
+	
 	public static void saveConfig() {
 		try {
 			config.save(configFile);
-		} catch (IOException e) {
+		} catch (Exception e) {
+			System.out.println("Alles wurde gespeichert");
+			e.printStackTrace();
 		}
 	}
 
